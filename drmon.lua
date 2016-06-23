ae7f6d353e7ab8e0bd7132862dd1ab6c967b4cb3
kevin olson <acidjazz@gmail.com>
2016-06-22 17:26:42 -0700

    manual input func

--- drmon.lua
+++ drmon.lua
@@ -4,11 +4,18 @@ local fluxgateSide = "right"
 
 local targetStrength = 50
 local maxTemperature = 8000
+local safeTemperature = 3000
+local lowestFieldPercent = 15
 
-local activateOnCharged = true
+local activateOnCharged = 1
 
--- please leave untouched from here on
+-- please leave things untouched from here on
 os.loadAPI("lib/f")
+
+local version = "0.25"
+-- toggleable via the monitor, use our algorithm to achieve our target field strength or let the user tweak it
+local autoInputGate = 1
+local curInputGate = 222000
 
 -- monitor 
 local mon, monitor, monX, monY
@@ -25,7 +32,6 @@ local ri
 local action = "None since reboot"
 local emergencyCharge = false
 local emergencyTemp = false
-
 
 monitor = f.periphSearch("monitor")
 inputfluxgate = f.periphSearch("flux_gate")
@@ -52,33 +58,107 @@ monX, monY = monitor.getSize()
 mon = {}
 mon.monitor,mon.X, mon.Y = monitor, monX, monY
 
+--write settings to config file
+function save_config()
+  sw = fs.open("config.txt", "w")   
+  sw.writeLine(version)
+  sw.writeLine(autoInputGate)
+  sw.writeLine(curInputGate)
+  sw.close()
+end
+
+--read settings from file
+function load_config()
+  sr = fs.open("config.txt", "r")
+  version = sr.readLine()
+  autoInputGate = tonumber(sr.readLine())
+  curInputGate = tonumber(sr.readLine())
+  sr.close()
+end
+
+
+-- 1st time? save our settings, if not, load our settings
+if fs.exists("config.txt") == false then
+  save_config()
+else
+  load_config()
+end
 
 function buttons()
+
   while true do
     -- button handler
     event, side, xPos, yPos = os.pullEvent("monitor_touch")
-    -- error(event .. ":" .. side .. ":" .. xPos .. ":" .. yPos)
-    -- 2,8-4,8 = -1000, 6,8-9,8 = -10000, 11,8-13,8 = -100000
-    -- 16,8-18,8 = +1000, 20,8-24,8 = +10000, 25,8-28,8 = +100000
+
+    -- output gate controls
+    -- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
+    -- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
     if yPos == 8 then
       local cFlow = fluxgate.getSignalLowFlow()
       if xPos >= 2 and xPos <= 4 then
         cFlow = cFlow-1000
       elseif xPos >= 6 and xPos <= 9 then
         cFlow = cFlow-10000
-      elseif xPos >= 11 and xPos <= 13 then
+      elseif xPos >= 10 and xPos <= 12 then
         cFlow = cFlow-100000
-      elseif xPos >= 16 and xPos <= 18 then
+      elseif xPos >= 17 and xPos <= 19 then
         cFlow = cFlow+100000
-      elseif xPos >= 20 and xPos <= 24 then
+      elseif xPos >= 21 and xPos <= 23 then
         cFlow = cFlow+10000
-      elseif xPos >= 25 and xPos <= 28 then
+      elseif xPos >= 25 and xPos <= 27 then
         cFlow = cFlow+1000
       end
       fluxgate.setSignalLowFlow(cFlow)
     end
+
+    -- input gate controls
+    -- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
+    -- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
+    if yPos == 10 and autoInputGate == 0 and xPos ~= 14 and xPos ~= 15 then
+      if xPos >= 2 and xPos <= 4 then
+        curInputGate = curInputGate-1000
+      elseif xPos >= 6 and xPos <= 9 then
+        curInputGate = curInputGate-10000
+      elseif xPos >= 10 and xPos <= 12 then
+        curInputGate = curInputGate-100000
+      elseif xPos >= 17 and xPos <= 19 then
+        curInputGate = curInputGate+100000
+      elseif xPos >= 21 and xPos <= 23 then
+        curInputGate = curInputGate+10000
+      elseif xPos >= 25 and xPos <= 27 then
+        curInputGate = curInputGate+1000
+      end
+      inputfluxgate.setSignalLowFlow(curInputGate)
+      save_config()
+    end
+
+    -- input gate toggle
+    if yPos == 10 and ( xPos == 14 or xPos == 15) then
+      if autoInputGate == 1 then
+        autoInputGate = 0
+      else
+        autoInputGate = 1
+      end
+      save_config()
+    end
+
   end
 end
+
+function drawButtons(y)
+
+  -- 2-4 = -1000, 6-9 = -10000, 10-12,8 = -100000
+  -- 17-19 = +1000, 21-23 = +10000, 25-27 = +100000
+
+  f.draw_text(mon, 2, y, " < ", colors.white, colors.gray)
+  f.draw_text(mon, 6, y, " <<", colors.white, colors.gray)
+  f.draw_text(mon, 10, y, "<<<", colors.white, colors.gray)
+
+  f.draw_text(mon, 17, y, ">>>", colors.white, colors.gray)
+  f.draw_text(mon, 21, y, ">> ", colors.white, colors.gray)
+  f.draw_text(mon, 25, y, " > ", colors.white, colors.gray)
+end
+
 
 
 function update()
@@ -125,21 +205,16 @@ function update()
     f.draw_text_lr(mon, 2, 7, 1, "Output Gate", f.format_int(fluxgate.getSignalLowFlow()) .. " rf/t", colors.white, colors.blue, colors.black)
 
     -- buttons
-
-    -- 2,8-4,8 = -1000, 6,8-9,8 = -10000, 11,8-13,8 = -100000
-    -- 16,8-18,8 = +1000, 20,8-24,8 = +10000, 25,8-28,8 = +100000
-    f.draw_text(mon, 2, 8, " < ", colors.white, colors.gray)
-    f.draw_text(mon, 6, 8, " << ", colors.white, colors.gray)
-    f.draw_text(mon, 11, 8, "<<<", colors.white, colors.gray)
-
-    f.draw_text(mon, 16, 8, ">>>", colors.white, colors.gray)
-    f.draw_text(mon, 20, 8, " >> ", colors.white, colors.gray)
-    f.draw_text(mon, 25, 8, " > ", colors.white, colors.gray)
-
-
-
+    drawButtons(8)
 
     f.draw_text_lr(mon, 2, 9, 1, "Input Gate", f.format_int(inputfluxgate.getSignalLowFlow()) .. " rf/t", colors.white, colors.blue, colors.black)
+
+    if autoInputGate == 1 then
+      f.draw_text(mon, 14, 10, "AU", colors.white, colors.gray)
+    else
+      f.draw_text(mon, 14, 10, "MA", colors.white, colors.gray)
+      drawButtons(10)
+    end
 
     local satPercent
     satPercent = math.ceil(ri.energySaturation * 0.0000001 * 100)*.01
@@ -154,7 +229,11 @@ function update()
     if fieldPercent > 70 then fieldColor = colors.red end
     if fieldPercent < 70 and fieldPercent > 30 then fieldColor = colors.orange end
 
-    f.draw_text_lr(mon, 2, 14, 1, "Field Strength", fieldPercent .. "%", colors.white, fieldColor, colors.black)
+    if autoInputGate == 1 then 
+      f.draw_text_lr(mon, 2, 14, 1, "Field Strength T:" .. targetStrength, fieldPercent .. "%", colors.white, fieldColor, colors.black)
+    else
+      f.draw_text_lr(mon, 2, 14, 1, "Field Strength", fieldPercent .. "%", colors.white, fieldColor, colors.black)
+    end
     f.progress_bar(mon, 2, 15, mon.X-2, fieldPercent, 100, fieldColor, colors.gray)
 
     local fuelPercent, fuelColor
@@ -184,21 +263,26 @@ function update()
     end
 
     -- are we stopping from a shutdown and our temp is better? activate
-    if emergencyTemp == true and ri.status == "stopping" and ri.temperature < 3000 then
+    if emergencyTemp == true and ri.status == "stopping" and ri.temperature < safeTemperature then
       reactor.activateReactor()
       emergencyTemp = false
     end
 
     -- are we charged? lets activate
-    if ri.status == "charged" and activateOnCharged == true then
+    if ri.status == "charged" and activateOnCharged == 1 then
       reactor.activateReactor()
     end
 
     -- are we on? regulate the input fludgate to our target field strength
+    -- or set it to our saved setting since we are on manual
     if ri.status == "online" then
-      fluxval = ri.fieldDrainRate / (1 - (targetStrength/100) )
-      print("Target Gate: ".. fluxval)
-      inputfluxgate.setSignalLowFlow(fluxval)
+      if autoInputGate == 1 then 
+        fluxval = ri.fieldDrainRate / (1 - (targetStrength/100) )
+        print("Target Gate: ".. fluxval)
+        inputfluxgate.setSignalLowFlow(fluxval)
+      else
+        inputfluxgate.setSignalLowFlow(curInputGate)
+      end
     end
 
     -- safeguards
@@ -211,13 +295,14 @@ function update()
     end
 
     -- field strength is too dangerous, kill and it try and charge it before it blows
-    if fieldPercent <= 15 and ri.status == "online" then
-      action = "Field Str < 15%"
+    if fieldPercent <= lowestFieldPercent and ri.status == "online" then
+      action = "Field Str < " ..lowestFieldPercent.."%"
       reactor.stopReactor()
       reactor.chargeReactor()
       emergencyCharge = true
     end
 
+    -- temperature too high, kill it and activate it when its cool
     if ri.temperature > maxTemperature then
       reactor.stopReactor()
       action = "Temp > " .. maxTemperature
@@ -229,3 +314,4 @@ function update()
 end
 
 parallel.waitForAny(buttons, update)
+
