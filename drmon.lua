cdbc7ae09ca4c6d070e272347aee98b4ce5c6b3c
Chris <christopherfeger@yahoo.com>
2016-12-11 14:12:54 -0500

    Fixed problems when max field strength < 100 million

--- drmon.lua
+++ drmon.lua
@@ -223,7 +223,7 @@ function update()
     f.progress_bar(mon, 2, 12, mon.X-2, satPercent, 100, colors.blue, colors.gray)
 
     local fieldPercent, fieldColor
-    fieldPercent = math.ceil(ri.fieldStrength * 0.000001 * 100)*.01
+    fieldPercent = math.ceil(ri.fieldStrength / ri.maxFieldStrength * 10000)*.01
 
     fieldColor = colors.red
     if fieldPercent > 50 then fieldColor = colors.green end
